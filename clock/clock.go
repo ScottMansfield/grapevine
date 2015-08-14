7c700c69698c723e0a7ac8941452b89a2a0587ec
Scott Mansfield <sgmansf@gmail.com>
2015-08-13 23:22:46 -0700

    Adding asynchronous clock interactions
    All access is serialized through the use of a select over multiple
    channels. A consistent view is help by only having one clock in one
    place, which is copied when handed out.

--- clock/clock.go
+++ clock/clock.go
@@ -1,13 +1,62 @@
 /**
  * Vector clock implementation
  */
-package clock;
+package clock
 
+import "math"
+
+// Model and instance
 type Clock struct {
-    // wtf do I put here
-    size int
+    // TODO: wtf do I put here
+    Size int
+    Epoch int64
 }
 
-func (c *Clock) Size() int {
-    return c.size;
+var clock *Clock
+
+// Operation queues and message structs
+type UpdateMsg struct {
+    Respond chan Clock
+}
+var Update chan UpdateMsg
+
+type SetMsg struct {
+    Epoch int64
+    Respond chan Clock
+}
+var Set chan SetMsg
+
+type GetMsg struct {
+    Respond chan Clock
+}
+var Get chan GetMsg
+
+func init() {
+    // init channels
+    Update = make(chan UpdateMsg)
+    Set = make(chan SetMsg)
+    Get = make(chan GetMsg)
+    
+    // Create clock manager
+    go clockManager()
+}
+
+// Serializes all interaction with the clock
+// I'm sure there's some kind of insidious bug hidden
+// in this code somewhere...
+func clockManager() {
+    var clock Clock
+    
+    for {
+        select {
+            case updateReq := <-Update:
+                clock.Epoch += 1
+                updateReq.Respond <- clock
+            case setReq := <-Set:
+                clock.Epoch = int64(math.Max(float64(clock.Epoch), float64(setReq.Epoch)))
+                setReq.Respond <- clock
+            case getReq := <-Get:
+                getReq.Respond <- clock
+        }
+    }
 }
