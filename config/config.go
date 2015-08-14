b8027a6ceb4e2b347a4f1939cb7a6c829e53e9a5
Scott Mansfield <sgmansf@gmail.com>
2015-08-13 22:38:55 -0700

    Adding more config fields, enhancing parsing a bit

--- config/config.go
+++ config/config.go
@@ -1,8 +1,57 @@
 /**
  * Config model
  */
-package config;
+package config
+
+import "bufio"
+import "fmt"
+import "os"
+import "strconv"
+import "strings"
 
 type Config struct {
-    Seeds []string
+    Seeds          []string
+    Port           int
+    GossipInterval int
+    SyncInterval   int
+}
+
+func Parse(fileName string) (Config, error) {
+    fmt.Println("Parsing", fileName)
+    var retval Config
+    
+    file, err := os.Open(fileName)
+    if err != nil { return retval, err }
+    defer file.Close()
+    
+    scanner := bufio.NewScanner(file)
+    
+    for scanner.Scan() {
+        line := strings.TrimSpace(scanner.Text())
+        
+        if strings.HasPrefix(line, "#") || line == "" {
+            continue
+        }
+        
+        parts := strings.SplitN(line, "=", 2)
+        
+        switch strings.TrimSpace(parts[0]) {
+            case "seeds":
+                retval.Seeds = strings.Split(strings.TrimSpace(parts[1]), ",")
+            case "port":
+                val, err := strconv.Atoi(strings.TrimSpace(parts[1]))
+                if err != nil { return retval, err }
+                retval.Port = val
+            case "gossipInterval":
+                val, err := strconv.Atoi(strings.TrimSpace(parts[1]))
+                if err != nil { return retval, err }
+                retval.GossipInterval = val
+            case "syncInterval":
+                val, err := strconv.Atoi(strings.TrimSpace(parts[1]))
+                if err != nil { return retval, err }
+                retval.SyncInterval = val
+        }
+    }
+    
+    return retval, nil
 }
